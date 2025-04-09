65d8ea9c65ca6118995c16460adcb8ba6d209365
hieutran1220 <156657159+hieutran1220@users.noreply.github.com>
2025-04-09 16:18:49 +0700

    navigation and route

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -4,32 +4,60 @@ import { Tabs } from "expo-router";
 import { images } from "@/constants/images";
 import { icons } from "@/constants/icons";
 
+const TabIcon = ({ focused, icon, title }: any) => {
+  if (focused) {
+    return (
+      <ImageBackground
+        source={images.highlight}
+        className="flex flex-row w-full flex-1
+                   min-w-[112px] min-h-16 mt-4 justify-center
+                    items-center rounded-full overflow-hidden"
+      >
+        <Image source={icon} tintColor="#151312" className="size-5" />
+        <Text className="text-secondary text-base font-semibold ml-2">
+          {title}
+        </Text>
+      </ImageBackground>
+    );
+  }
+  return (
+    <View className="size-full justify-center items-center mt-4 rounded-full">
+      <Image source={icon} tintColor="#A8B5DB" className="size-5" />
+    </View>
+  );
+};
+
 const _layout = () => {
   return (
-    <Tabs>
+    <Tabs
+      screenOptions={{
+        tabBarShowLabel: false,
+        tabBarItemStyle: {
+          width: "100%",
+          height: "100%",
+          justifyContent: "center",
+          alignItems: "center",
+        },
+        tabBarStyle: {
+          backgroundColor: "#0f0D23",
+          borderRadius: 50,
+          marginHorizontal: 20,
+          marginBottom: 36,
+          height: 52,
+          width: "90%",
+          position: "absolute",
+          overflow: "hidden",
+          borderWidth: 1,
+        },
+      }}
+    >
       <Tabs.Screen
         name="index"
         options={{
           title: "Home",
           headerShown: false,
           tabBarIcon: ({ focused }: { focused: boolean }) => (
-            <>
-              <ImageBackground
-                source={images.highlight}
-                className="flex flex-row w-full flex-1
-                 min-w-[112px] min-h-12 mt-4 justify-center
-                  items-center rounded-full overflow-hidden"
-              >
-                <Image
-                  source={icons.home}
-                  tintColor="#151312"
-                  className="size-5"
-                />
-                <Text className="text-secondary text-base font-semibold ml-2">
-                  Home
-                </Text>
-              </ImageBackground>
-            </>
+            <TabIcon focused={focused} icon={icons.home} title="Home" />
           ),
         }}
       />
@@ -38,6 +66,9 @@ const _layout = () => {
         options={{
           title: "Search",
           headerShown: false,
+          tabBarIcon: ({ focused }: { focused: boolean }) => (
+            <TabIcon focused={focused} icon={icons.search} title="Search" />
+          ),
         }}
       />
       <Tabs.Screen
@@ -45,6 +76,9 @@ const _layout = () => {
         options={{
           title: "Saved",
           headerShown: false,
+          tabBarIcon: ({ focused }: { focused: boolean }) => (
+            <TabIcon focused={focused} icon={icons.save} title="Saved" />
+          ),
         }}
       />
       <Tabs.Screen
@@ -52,6 +86,9 @@ const _layout = () => {
         options={{
           title: "Profile",
           headerShown: false,
+          tabBarIcon: ({ focused }: { focused: boolean }) => (
+            <TabIcon focused={focused} icon={icons.person} title="Profile" />
+          ),
         }}
       />
     </Tabs>
---
+++ services/api.ts
@@ -0,0 +1,13 @@
+// const url = 'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc';
+// const options = {
+//   method: 'GET',
+//   headers: {
+//     accept: 'application/json',
+//     Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlNTM4OGU4M2EzOGM1NGM1YWY4YjQ1MDM4OWEzYzRkOCIsIm5iZiI6MTc0NDE4ODA1My4wNzcsInN1YiI6IjY3ZjYzMjk1ZWE4MGQ4NTE3NTk5MjkwNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.p5HF6gSA-VrFfFE-i5-MDwXa5xqDWLjMyitk1xGOOwQ'
+//   }
+// };
+
+// fetch(url, options)
+//   .then(res => res.json())
+//   .then(json => console.log(json))
+//   .catch(err => console.error(err));
